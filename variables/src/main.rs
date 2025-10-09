56a34eab177c4daff349181c9f02f75f61280c78
alice <ryutoiwa2929@gmail.com>
2025-10-09 20:28:00 +0900

    add

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     let mut x = 5;
+    const CONSTANT: usize = 100;
+    println!("The value of CONSTANT is: {}", CONSTANT);
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
