c5f34cb2b7db24947978a474fd5a61da3aca4e30
alice <ryutoiwa2929@gmail.com>
2025-10-09 22:08:57 +0900

    add

--- STRUCT/src/main.rs
+++ STRUCT/src/main.rs
@@ -1,8 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
 
-fn build_user(email: String, username: String) {
+fn main() {
+
+    let user1 = build_user("email1".to_string(), "lemolatoon1".to_string());
+
+    println!("{:?}", &user1);
+    
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    println!("{:?}", user2);
+}
+
+fn build_user(email: String, username: String) -> User {
     User {
         email,
         username,
