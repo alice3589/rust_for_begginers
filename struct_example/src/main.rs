4d871a2c085442a4de2e24d36526f64df89fc7e8
alice <ryutoiwa2929@gmail.com>
2025-10-09 22:20:38 +0900

    add

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,14 +1,21 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let width = 30;
-    let height = 50;
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    }
 
     println!(
         // 長方形の面積は、{}平方ピクセルです。
         "The area of the rectangle is {} square pixels.",
-        area(width, height)
+        area((width, height))
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
