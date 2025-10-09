8f7f8cfa3d716c9bb644d65eb211c04a976e14a4
alice <ryutoiwa2929@gmail.com>
2025-10-09 22:34:59 +0900

    foo

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -6,10 +6,17 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(width: u32) -> Self {
+        Self {
+            width,
+            height: width
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
     }
-    }
+}
 
 fn main() {
     let rect = Rectangle {
@@ -17,13 +24,16 @@ fn main() {
         height: 50,
     };
 
+    let square = Rectangle::square(60);
+
     println!(
         // 長方形の面積は、{}平方ピクセルです。
         "The area of the rectangle is {} square pixels.",
-        rect.area()
+        square.area()
     );
 
     println!("{:?}", rect);
+    println!("{:?}", square);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
