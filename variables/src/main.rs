2cfaa049c21306d6f1195a0f7b285ee3d74561f7
alice <ryutoiwa2929@gmail.com>
2025-10-09 20:32:25 +0900

    add

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,15 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let y = 5; // 5
+    
+    let y = y + 1; // 6
+
+    {
+        let y = y * 2; // 12
+        println!("The value of y in the inner scope is: {}", y);
+    }
+
+    println!("The value of y is: {}", y);
 }
