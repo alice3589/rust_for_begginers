36e01441c7781be9885d660cc08efd209d35eeb4
alice <ryutoiwa2929@gmail.com>
2025-10-10 00:34:06 +0900

    add

---
+++ struct_example/target/debug/deps/struct_example.d
@@ -0,0 +1,5 @@
+C:\Users\iwary\rust_for_begginers\struct_example\target\debug\deps\struct_example.d: src\main.rs
+
+C:\Users\iwary\rust_for_begginers\struct_example\target\debug\deps\struct_example.exe: src\main.rs
+
+src\main.rs:
---
+++ struct_example/target/debug/struct_example.d
@@ -0,0 +1,1 @@
+C:\Users\iwary\rust_for_begginers\struct_example\target\debug\struct_example.exe: C:\Users\iwary\rust_for_begginers\struct_example\src\main.rs
