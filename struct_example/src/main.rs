b8487f7c8b734d95bf638dd6b8f8a411f770247c
alice <ryutoiwa2929@gmail.com>
2025-10-09 22:25:33 +0900

    bar

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+
 struct Rectangle {
     width: u32,
     height: u32,
@@ -22,6 +24,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(&rect)
     );
+
+    println!("{:?}", rect);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
