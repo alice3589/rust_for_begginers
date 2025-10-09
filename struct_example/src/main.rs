6cd34bb22a0b9a3c1254a35ec53db7ab6da96f6c
alice <ryutoiwa2929@gmail.com>
2025-10-09 22:21:20 +0900

    foo

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let rect = Rectangle {
         width: 30,
         height: 50,
-    }
+    };
 
     println!(
         // 長方形の面積は、{}平方ピクセルです。
         "The area of the rectangle is {} square pixels.",
-        area((width, height))
+        area(rect)
     );
 }
 
