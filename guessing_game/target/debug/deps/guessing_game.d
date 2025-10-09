64d1cbc608300d6dd6059d2259305f0db89df491
alice <ryutoiwa2929@gmail.com>
2025-10-10 00:14:59 +0900

    add

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\iwary\rust_for_begginers\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+C:\Users\iwary\rust_for_begginers\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\iwary\rust_for_begginers\guessing_game\target\debug\guessing_game.exe: C:\Users\iwary\rust_for_begginers\guessing_game\src\main.rs
