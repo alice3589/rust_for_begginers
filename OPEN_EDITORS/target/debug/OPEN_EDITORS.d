f2a5a280e9f805e04fc2d4222af5f35aa4ea853a
alice <ryutoiwa2929@gmail.com>
2025-10-11 23:47:05 +0900

    add

---
+++ OPEN_EDITORS/target/debug/OPEN_EDITORS.d
@@ -0,0 +1,1 @@
+C:\Users\iwary\rust_for_begginers\OPEN_EDITORS\target\debug\OPEN_EDITORS.exe: C:\Users\iwary\rust_for_begginers\OPEN_EDITORS\src\main.rs
---
+++ OPEN_EDITORS/target/debug/deps/OPEN_EDITORS.d
@@ -0,0 +1,5 @@
+C:\Users\iwary\rust_for_begginers\OPEN_EDITORS\target\debug\deps\OPEN_EDITORS.d: src\main.rs
+
+C:\Users\iwary\rust_for_begginers\OPEN_EDITORS\target\debug\deps\OPEN_EDITORS.exe: src\main.rs
+
+src\main.rs:
