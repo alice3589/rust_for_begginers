80051cef7291b62e45b5492aee5c888244a9cb80
alice <ryutoiwa2929@gmail.com>
2025-10-12 22:30:07 +0900

    add

--- MATCH_EXAMPLE/src/main.rs
+++ MATCH_EXAMPLE/src/main.rs
@@ -4,12 +4,30 @@ enum Color {
     Red,
     Blue,
     Green,
+    Yellow,
 }
 
 fn main() {
     println!("Hello, world!");
     let green = Color::Green;
+    let yellow = Color::Yellow;
     println!("Green Color Code: {}", color_to_str(green));
+    println!("Yellow Color Code {}", color_to_str(yellow));
+    find_maybe_number(Some(5));
+    find_maybe_number(None);
+
+}
+
+// enum Option {
+//     Some(u32),
+//     None,
+// }
+
+fn find_maybe_number(maybe_number: Option<u32>) {
+    match maybe_number {
+        Some(number) => println!("found {}", number),
+        None => println!("Nothing found."),
+    }
 }
 
 fn color_to_str(color: Color) -> String {
@@ -20,5 +38,6 @@ fn color_to_str(color: Color) -> String {
         Color::Red => "#FF0000".to_string(),
         Color::Blue => "#0000FF".to_string(),
         Color::Green => "#00FF00".to_string(),
+        Color::Yellow => "#FFFF00".to_string(),
     }
 }
