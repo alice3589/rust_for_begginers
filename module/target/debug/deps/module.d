4f4eb47324a25d36df9182c89aea58c30b3da25d
alice <ryutoiwa2929@gmail.com>
2025-10-20 22:59:18 +0900

    add

--- module/target/debug/deps/module.d
+++ module/target/debug/deps/module.d
@@ -1,6 +1,8 @@
-C:\Users\iwary\rust_for_begginers\module\target\debug\deps\module.d: src\main.rs src\second.rs
+C:\Users\iwary\rust_for_begginers\module\target\debug\deps\module.d: src\main.rs src\second.rs src\third.rs src\third\return_three.rs
 
-C:\Users\iwary\rust_for_begginers\module\target\debug\deps\module.exe: src\main.rs src\second.rs
+C:\Users\iwary\rust_for_begginers\module\target\debug\deps\module.exe: src\main.rs src\second.rs src\third.rs src\third\return_three.rs
 
 src\main.rs:
 src\second.rs:
+src\third.rs:
+src\third\return_three.rs:
--- module/target/debug/module.d
+++ module/target/debug/module.d
@@ -1,1 +1,1 @@
-C:\Users\iwary\rust_for_begginers\module\target\debug\module.exe: C:\Users\iwary\rust_for_begginers\module\src\main.rs C:\Users\iwary\rust_for_begginers\module\src\second.rs
+C:\Users\iwary\rust_for_begginers\module\target\debug\module.exe: C:\Users\iwary\rust_for_begginers\module\src\main.rs C:\Users\iwary\rust_for_begginers\module\src\second.rs C:\Users\iwary\rust_for_begginers\module\src\third\return_three.rs C:\Users\iwary\rust_for_begginers\module\src\third.rs
